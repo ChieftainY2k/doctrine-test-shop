cbaccd53be867dcf4a0244b91c70876c274907cd
Tomasz <tomasz.rozdzynski@codete.co>
2016-04-14 17:41:48 +0200

    logger added

--- public/test-categories.php
+++ public/test-categories.php
@@ -5,25 +5,29 @@ require "../bootstrap.php";
 $em = ApplicationConfiguration::getEntityManager();
 //print_r($em);
 
+$logger = new \Doctrine\DBAL\Logging\EchoSQLLogger();
+$em->getConnection()
+    ->getConfiguration()
+    ->setSQLLogger($logger);
+
 $categoryRepository = $em->getRepository('\Entity\Shop\Product\Category');
 $categories = $categoryRepository->findAll();
 
+
 foreach ($categories as $category) {
-    /* @var \Entity\Shop\Product\Category $category  */
+    /* @var \Entity\Shop\Product\Category $category */
     echo "<hr>Name: " . $category->getName();
     $children = $category->getChildren();
     $parent = $category->getParent();
-    if (!empty($parent))
-    {
-        echo "<br>parent: ".$parent->getName();
+    if (!empty($parent)) {
+        echo "<br>parent: " . $parent->getName();
     }
 
-    if (!empty($children))
-    {
-        foreach($children as $child)
-        {
-            /* @var \Entity\Shop\Product\Category $child  */
-            echo "<br>child: ".$child->getName();
+    if (!empty($children)) {
+        foreach ($children as $child) {
+
+            /* @var \Entity\Shop\Product\Category $child */
+            echo "<br>child: " . $child->getName();
         }
     }
 
