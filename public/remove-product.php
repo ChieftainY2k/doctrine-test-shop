3e68189ba9b82df1fe94ce7a589e3037ade208d1
Tomasz <tomasz.rozdzynski@codete.co>
2016-04-15 14:29:41 +0200

    remove product

---
+++ public/remove-product.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ */
+function run()
+{
+    require "../bootstrap.php";
+
+    $em = ApplicationConfiguration::getEntityManager();
+    $em->getConnection()->getConfiguration()->setSQLLogger(new \Tools\DoctrineLogger());
+
+    $productId = 2;
+
+    $productRepository = $em->getRepository(\Entity\Shop\Product\Product::class);
+    $product = $productRepository->find($productId);
+    if (!$product )
+    {
+        throw new Exception("Cannot find product.");
+    }
+
+    $em->remove($product);
+
+    $em->flush();
+
+    echo "Product #$productId removed.";
+
+};
+
+run();
