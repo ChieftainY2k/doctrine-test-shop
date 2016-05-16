1d0d3b3e4bf81441ce724a6fb410234046e77f65
Tomasz <tomasz.rozdzynski@codete.co>
2016-05-16 14:10:13 +0200

    fix

--- public/list-barcodes-dto.php
+++ public/list-barcodes-dto.php
@@ -12,9 +12,9 @@ function run()
     $productId = !empty($_GET['id']) ? $_GET['id'] : 1;
 
     $query = $em->createQuery('SELECT NEW Dto\BarcodeDto(b.value) FROM Entity\Shop\Product\Barcode b');
-    $barcodes = $query->getResult(); // array of CustomerDTO
+    $barcodes = $query->getResult(); // array of custom DTOs
 
-    echo "<pre>"; print_R($barcodes); echo "</pre>";
+    echo "<pre>"; print_r($barcodes); echo "</pre>";
 
 }
 
